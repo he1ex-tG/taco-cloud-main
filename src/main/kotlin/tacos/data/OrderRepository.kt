b1e86904f2385a2297db91b1e882818dfdeb6300
User <he1ex-tg@yandex.ru>
2023-01-31 16:55:49 +0500

    Resolve merge conflict #2

--- src/main/kotlin/tacos/data/OrderRepository.kt
+++ src/main/kotlin/tacos/data/OrderRepository.kt
@@ -3,8 +3,4 @@ package tacos.data
 import org.springframework.data.repository.CrudRepository
 import tacos.TacoOrder
 
-<<<<<<< HEAD
 interface OrderRepository : CrudRepository<TacoOrder, String>
-=======
-interface OrderRepository : CrudRepository<TacoOrder, Long>
->>>>>>> master
