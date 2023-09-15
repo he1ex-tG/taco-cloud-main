9eaf6014b3ae09979b06645513fdfc45db4165f4
User <he1ex-tg@yandex.ru>
2023-09-15 17:03:36 +0500

    Change security configuration

--- src/main/kotlin/tacos/security/SecurityConfig.kt
+++ src/main/kotlin/tacos/security/SecurityConfig.kt
@@ -3,16 +3,12 @@ package tacos.security
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
-import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.web.servlet.invoke
-import org.springframework.security.core.userdetails.UserDetailsService
-import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
-import tacos.data.UserRepository
 
 @Configuration
 @EnableWebSecurity
@@ -20,41 +16,28 @@ class SecurityConfig {
 
     @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-        http.invoke {
+        http {
+            csrf {
+                disable()
+            }
             authorizeRequests {
-                //authorize("/design/**", hasRole("USER"))
-                authorize("/design/**", hasAuthority("SCOPE_tacoMain"))
-                //authorize("/orders/**", hasRole("USER"))
-                authorize("/orders/**", hasAuthority("SCOPE_tacoMain"))
-                authorize(HttpMethod.POST, "/api/ingredients/**", hasAuthority("SCOPE_writeIngredients"))
-                authorize(HttpMethod.DELETE, "/api/ingredients/**", hasAuthority("SCOPE_deleteIngredients"))
+                authorize("/design/**", hasAuthority("SCOPE_taco:user"))
+                authorize("/orders/**", hasAuthority("SCOPE_taco:user"))
+                authorize(HttpMethod.POST, "/api/ingredients/**", hasAuthority("SCOPE_write:ingredients"))
+                authorize(HttpMethod.DELETE, "/api/ingredients/**", hasAuthority("SCOPE_delete:ingredients"))
                 authorize("/", permitAll)
                 authorize("/**", permitAll)
             }
-            formLogin {
-                loginPage = "/login"
-                defaultSuccessUrl("/design", true)
-            }
-            logout {
-                logoutSuccessUrl = "/login"
-            }
             oauth2Login {
-                loginPage = "/login"
+                loginPage = "/oauth2/authorization/taco-user-client"
             }
-
+            oauth2ResourceServer {
+                jwt {
+                    jwkSetUri = "http://authserver:9000/oauth2/v1/jwks"
+                }
+            }
         }
         return http.build()
-    }
-
-    @Bean
-    fun userDetailService(userRepository: UserRepository): UserDetailsService {
-        return UserDetailsService {
-            username ->
-                val user = userRepository.findUserByUusername(username)
-                user.orElseThrow {
-                    UsernameNotFoundException("User \"$username\" not found.")
-                }
-        }
     }
 
     @Bean
